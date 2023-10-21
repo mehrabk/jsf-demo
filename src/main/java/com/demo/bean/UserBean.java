c87cd12e4185dc12747f2ce4aa79fedb4243fb08
m_kor <m_kor@modernisc.com>
2023-10-21 11:03:30 +0330

    jsf facelet templating

--- src/main/java/com/demo/bean/UserBean.java
+++ src/main/java/com/demo/bean/UserBean.java
@@ -2,6 +2,7 @@ package com.demo.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.validation.constraints.Size;
 
 /**
  * @author m_kor
@@ -13,6 +14,11 @@ public class UserBean {
 
     private String firstName;
     private String lastName;
+    private Long age;
+    private Double productPrice;
+    private String email;
+    @Size(min = 3, message = "fatherName should at least be 3 characters long")
+    private String fatherName;
 
     public String getFirstName() {
         return firstName;
@@ -28,5 +34,37 @@ public class UserBean {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
     }
 }
