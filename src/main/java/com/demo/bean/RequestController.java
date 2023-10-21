11b078fcee3b776d7f132dbb603574dea67b87a3
m_kor <m_kor@modernisc.com>
2023-10-21 11:49:40 +0330

    access request param using managed property annotation

--- src/main/java/com/demo/bean/RequestController.java
+++ src/main/java/com/demo/bean/RequestController.java
@@ -1,6 +1,7 @@
 package com.demo.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import java.util.Map;
@@ -16,11 +17,20 @@ public class RequestController {
     private String param1;
     private String param2;
 
+    @ManagedProperty(value = "#{param.paramMP1Name}")
+    private String param1MP;
+    @ManagedProperty(value = "#{param.paramMP2Name}")
+    private String param2MP;
+
     public String parametersAction(){
         FacesContext fc = FacesContext.getCurrentInstance();
         Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
         param1 = params.get("param1Name");
         param2 = params.get("param2Name");
+        return "request-parameters-result";
+    }
+
+    public String parametersManagedPropertyAction(){
         return "request-parameters-result";
     }
 
@@ -32,4 +42,20 @@ public class RequestController {
         return param2;
     }
 
+
+    public String getParam1MP() {
+        return param1MP;
+    }
+
+    public void setParam1MP(String param1MP) {
+        this.param1MP = param1MP;
+    }
+
+    public String getParam2MP() {
+        return param2MP;
+    }
+
+    public void setParam2MP(String param2MP) {
+        this.param2MP = param2MP;
+    }
 }
